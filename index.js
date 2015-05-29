369f403e05c710e40018b36e55e0530f2fa57d05
Kirill Fomichev <fanatid@ya.ru>
2015-05-29 10:42:14 +0300

    switch from one promise to array

--- index.js
+++ index.js
@@ -1,63 +1,114 @@
+/**
+ * Queue of promises, solve next problem:
+ *   var promise = require('bluebird').resolve()
+ *   console.log('mem usage (before):', process.memoryUsage())
+ *   function noop () {}
+ *   for (var i = 0; i < 100000; ++i) { promise.then(noop) }
+ *   console.log('mem usage (after):', process.memoryUsage())
+ */
+
 function getReadyMixin (Promise) {
   return function readyMixin (object) {
-    object._initReadyPromise = function _initReadyPromise () {
-      var self = this
-      self._isReady = false
-      self._readyPromise = new Promise(function (resolve, reject) {
-        self._readyResolve = resolve
-        self._readyReject = reject
-      })
+    object._readyMixinInitialize = function _readyMixinInitialize () {
+      this._readyMixinState = {
+        status: null,
+        reason: null,
+        value: null,
+        promises: []
+      }
     }
 
     object._ready = function _ready (err /*, ... */) {
-      if (this._readyPromise === undefined) {
-        this._initReadyPromise()
+      var self = this
+
+      // initialize if not yet
+      if (self._readyMixinState === undefined) {
+        self._readyMixinInitialize()
       }
 
+      // return if already resolved or rejected
+      if (self._readyMixinState.status !== null) {
+        return
+      }
+
+      // update status and reject all promises
       if (arguments.length > 0 && err !== null) {
-        return this._readyReject(err)
+        self._readyMixinState.status = 'reject'
+        self._readyMixinState.reason = err
+        self._readyMixinState.promises.forEach(function (promise) {
+          promise.reject(self._readyMixinState.reason)
+        })
+        self._readyMixinState.promises = []
+        return
       }
 
-      this._isReady = true
-
+      // update status and collect all values
+      self._readyMixinState.status = 'resolve'
       if (arguments.length <= 2) {
-        return this._readyResolve(arguments[1])
+        self._readyMixinState.value = arguments[1]
+      } else {
+        self._readyMixinState.value = new Array(arguments.length - 1)
+        for (var i = 0; i < self._readyMixinState.value.length; ++i) {
+          self._readyMixinState.value[i] = arguments[i + 1]
+        }
       }
 
-      var args = new Array(arguments.length - 1)
-      for (var i = 0; i < args.length; ++i) {
-        args[i] = arguments[i + 1]
-      }
-
-      this._readyResolve(args)
+      // resolve all promises
+      self._readyMixinState.promises.forEach(function (promise) {
+        promise.resolve(self._readyMixinState.value)
+      })
+      self._readyMixinState.promises = []
     }
 
     Object.defineProperty(object, 'ready', {
       configurable: true,
       enumerable: true,
       get: function () {
-        if (this._readyPromise === undefined) {
-          this._initReadyPromise()
+        var self = this
+
+        // initialize if not
+        if (self._readyMixinState === undefined) {
+          self._readyMixinInitialize()
         }
 
-        return this._readyPromise
+        if (self._readyMixinState.status === 'resolve') {
+          // resolve
+          return Promise.resolve(self._readyMixinState.value)
+
+        } else if (self._readyMixinState.status === 'reject') {
+          // reject
+          return Promise.reject(self._readyMixinState.reason)
+
+        } else {
+          // create new promise
+          return new Promise(function (resolve, reject) {
+            self._readyMixinState.promises.push({
+              resolve: resolve,
+              reject: reject
+            })
+          })
+        }
       }
     })
 
     object.onReady = function onReady (callback, opts) {
       function onResolve (value) {
+        // resolve with few values
         if (opts !== undefined && Object(opts).spread) {
           return callback.apply(null, [null].concat(value))
         }
 
+        // resolve with one value
         callback(null, value)
       }
 
+      // get promise
       return this.ready.then(onResolve, function (err) { callback(err) })
     }
 
     object.isReady = function () {
-      return this._isReady || false
+      var state = this._readyMixinState
+      return state && state.status === 'resolve' || false
     }
   }
 }
